28bd29c83438a0b8f7487b158fb5aee4b13086ec
Gabriel Kuroda <gabrielkuroda20@gmail.com>
2021-12-23 17:56:24 -0300

    Add Game Manager

--- UdemyRPG/Assets/Scripts/EssencialsLoader.cs
+++ UdemyRPG/Assets/Scripts/EssencialsLoader.cs
@@ -6,22 +6,27 @@ public class EssencialsLoader : MonoBehaviour
 {
     public GameObject UIScreen;
     public GameObject player;
+    public GameObject gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Verifica se a UI não existe
+        //Verifica se a UI nï¿½o existe
         if(UIFade.instance == null)
         {
             //Instancia a UI
             UIFade.instance = Instantiate(UIScreen).GetComponent<UIFade>();
         }
 
-        //Verifica se o player não existe
+        //Verifica se o player nï¿½o existe
         if (PlayerController.instance == null)
         {
             //Instancia o Player
             PlayerController.instance = Instantiate(player).GetComponent<PlayerController>();
+        }
+
+        if(GameManager.instance == null){
+            Instantiate(gameManager);
         }
     }
 
