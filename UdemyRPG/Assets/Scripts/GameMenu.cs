48d125b0ca3cd81203672415d409b80fb57ed1dd
Gabriel Kuroda <gabrielkuroda20@gmail.com>
2021-12-27 18:27:33 -0300

    Finalizado Tela Status

--- UdemyRPG/Assets/Scripts/GameMenu.cs
+++ UdemyRPG/Assets/Scripts/GameMenu.cs
@@ -13,6 +13,9 @@ public class GameMenu : MonoBehaviour
     public Slider[] expSlider;
     public Image[] charImage;
     public GameObject[] charStatHolder;
+    public GameObject[] statusButtons;
+    public Text statusName, statusHp,statusMp,statusStr,statusDef,statusWpnEqpd,statusWpnPwr,statusArmrEqpd,statusArmrPwr,statusExp;
+    public Image statusImage;
 
     // Start is called before the first frame update
     void Start()
@@ -73,6 +76,8 @@ public class GameMenu : MonoBehaviour
     }
 
     public void ToggleWindow(int windowNumber){
+        //Atualiza os Status na Main
+        updateMainStats();
         //Percorre o Array de janelas
         for(int i = 0; i < windows.Length; i++){
             //Verifica se a Janela corresponde com o botão
@@ -96,5 +101,38 @@ public class GameMenu : MonoBehaviour
         theMenu.SetActive(false);
         //Indica que o Menu não está aberto
         GameManager.instance.gameMenuOpen = false;
+    }
+
+    public void OpenStatus(){
+        //Atualiza os Status na Main
+        updateMainStats();
+        //Atualiza as informações de Status
+        StatusChar(0);
+        //Atualiza os Botões
+        for(int i = 0; i < statusButtons.Length; i++){
+            //Ativa e Desativa os botões
+            statusButtons[i].SetActive(playerStats[i].gameObject.activeInHierarchy);
+            //Set no nome do char
+            statusButtons[i].GetComponentInChildren<Text>().text = playerStats[i].charName;
+        }
+    }
+
+    public void StatusChar(int selected){
+        //Preenche a tela de status
+        statusName.text = playerStats[selected].charName;
+        statusHp.text = ""+ playerStats[selected].currentHP + "/" + playerStats[selected].maxHP;
+        statusMp.text = ""+ playerStats[selected].currentMP + "/" + playerStats[selected].maxMP;
+        statusStr.text = playerStats[selected].strength.ToString();
+        statusDef.text = playerStats[selected].defence.ToString();
+        if(playerStats[selected].equippedWpn != ""){
+            statusWpnEqpd.text = playerStats[selected].equippedWpn;
+        }
+        statusWpnPwr.text = playerStats[selected].wpnPwr.ToString();
+        if(playerStats[selected].equippedArmr != ""){
+            statusArmrEqpd.text = playerStats[selected].equippedArmr;
+        }
+        statusArmrPwr.text = playerStats[selected].armrPwr.ToString();
+        statusExp.text = (playerStats[selected].expToNextLevel[playerStats[selected].playerLevel] - playerStats[selected].currentEXP).ToString();
+        statusImage.sprite = playerStats[selected].charImage;
     }
 }
